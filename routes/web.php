ce548473ad44a4aab229229745ad8df8bd5a6241
Yurny <yyladino6@misena.edu.co>
2021-04-28 11:26:14 -0500

    Proyecto creado

--- routes/web.php
+++ routes/web.php
@@ -34,6 +34,13 @@ Route::get('variables', function(){
 });
 Route::get('arreglos', function(){
     //Arreglo: estructura de datos
+    //
+    // Fuertemente Tipados: Debe definir el tipo de dato
+    //                      No se puede cambiar el tipo de dato
+    //                      JAVA, .NET, GO
+    // Debilmente Tipados:  No se requiere definir el tipo de dato
+    //                      Puedes cambiar el tipo de dato
+    //                      PHP , JAVASCRIPT , PYTHON
     $estudiantes = [
     'AN' => "Ana",
     'MA' => "Maria",
@@ -41,4 +48,40 @@ Route::get('arreglos', function(){
     echo "<pre>";
     var_dump($estudiantes);
     echo "</pre>";
+});
+
+Route::get("paises", function(){
+
+
+    $paises= [
+    "Colombia" =>[
+                    "capital" => "Bogota",
+                    "Moneda" =>  "Peso",
+                    "poblacion" => 51
+                    ] ,
+    "Peru" =>[
+                    "capital" => "Lima",
+                    "Moneda" =>  "Sol",
+                    "poblacion" => 32
+                    ],
+    "Paraguay"=>[
+                     "capital" => "Asuncion",
+                     "Moneda" =>  "Guarani",
+                     "poblacion" => 7
+                    ] ];
+    foreach($paises as $nombre => $pais){
+        echo "<pre>";
+        echo "<h1>$nombre</h1>";
+        print_r($pais["capital"] ."<br  />" );
+        print_r($pais["nombre"] ."<br  />");
+        print_r($pais["moneda"] ."<br  />");
+        echo "</pre>";
+        echo "<hr />";
+
+
+    }
+
+
+
+
 });
